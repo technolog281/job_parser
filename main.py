2f00f5b869eac4500c470607b0d9ef0503542413
Matthew Galkovsky <dr.galkovsky2017@gmai.com>
2021-03-03 18:38:13 +0500

    test.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from hh_maxpage_counter import max_page_counter
+# from hh_maxpage_counter import max_page_counter
 
-max_page = max_page_counter()
-print(max_page)
+# max_page = max_page_counter()
+# print(max_page)
+
