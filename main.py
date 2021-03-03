8a861edb629810c59edb79fb869c92362d530f73
Matthew Galkovsky <dr.galkovsky2017@gmai.com>
2021-03-03 17:35:47 +0500

    test.

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+from hh_maxpage_counter import max_page_counter
 
-url = 'https://kurgan.hh.ru/search/vacancy'
-params = {'text': 'python',
-          'items_on_page': 100
-          }
-
-session = requests.session()
-
-headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
-              'application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-RU,en;q=0.9,ru-RU;q=0.8,ru;q=0.7,en-US;q=0.6',
-    'cache-control': 'max-age=0',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/88.0.4324.190 Safari/537.36 '
-}
-
-hh_req = session.get(url, headers=headers, params=params)  # Запрос к HH.RU страницы с вакансиями
-
-hh_soup = BeautifulSoup(hh_req.text, 'html.parser')  # Вытаскиваем HTML-разметку
-page_count = hh_soup.find_all('span', {'class': 'pager-item-not-in-short-range'})  # Находим блок кол-ва страниц
-
-links = []
-
-for page in page_count:
-    links.append(int(page.find('a').text))
-
-print(links[-1])
+max_page = max_page_counter()
+print(max_page)
