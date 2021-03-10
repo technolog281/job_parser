e9c9e87074385d7c3b4beb8cc3106667103a39fc
Matthew Galkovsky <dr.galkovsky2017@gmai.com>
2021-03-10 15:45:29 +0500

    test.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from hh_script import max_page_counter, extract_hh_jobs
-
+from stov_script import max_page_counter, extract_hh_jobs
+#
+# max_page = max_page_counter()
+#
+# extract_hh_jobs(max_page)
+#
+# print(extract_hh_jobs(max_page))
 max_page = max_page_counter()
 
 extract_hh_jobs(max_page)
--- stov_script.py
+++ stov_script.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = 'https://stackoverflow.com/jobs'
 params = {'q': 'python',
           'l': '',
-          'page': 0
+          'pg': 1
           }  # Query Параметры запроса, упрощает URL и работу с ним
 
 session = requests.session()  # Функция автоматически генерирует cookies
@@ -36,31 +36,24 @@ def max_page_counter():
     return int(links[-2])  # Вернули значение функции
 
 
+def extract_job(html):  # Собирает все спарсенные куски в словарь
+    job_title = html.find('a', {'class': 's-link stretched-link'})
+#   link = html.find('a')['href']
+#   company_name = html.find('h3', {'class': 'fc-black-700 fs-body1 mb4'})
+#   company_name = company_name.strip()  # Strip - удаляет символы в начале и в конце строки
+#   job_city = html.find('span', {'span': 'fc-black-500'})
+#   job_city = job_city.partition(',')[0]
+    return job_title
 
-  # links = []
-   # for page in sof_page_count:
-   #     links.append(int(page.find('a').text))  # Тянем из всех ссылок текст и форматируем в integer
-   #     return links[-1]  # Вернули значение функции
-#
-#
-# def extract_job(html):  # Собирает все спарсенные куски в словарь
-#     jobs_title = html.find('a').text
-#     link = html.find('a')['href']
-#     company_name = html.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).text
-#     company_name = company_name.strip()  # Strip - удаляет символы в начале и в конце строки
-#     job_city = html.find('span', {'data-qa': 'vacancy-serp__vacancy-address'}).text
-#     job_city = job_city.partition(',')[0]
-#     return {'job_title': jobs_title, 'company_name': company_name, 'location': job_city, 'link': link}
-#
-#
-# def extract_hh_jobs(last_page):
-#     jobs_title = []
-#     for page in range(last_page):
-#         print(f'Парсинг страницы {page}')
-#         result = session.get(url, headers=headers, params=dict(params, page=page))
-#         soup = BeautifulSoup(result.text, 'html.parser')
-#         results = soup.find_all('div', {'class': 'vacancy-serp-item'})
-#         for result in results:
-#             vacancy = extract_job(result)
-#             jobs_title.append(vacancy)
-#     return jobs_title
+
+def extract_hh_jobs(last_page):
+    jobs_title = []
+    for page in range(1):
+        print(f'Парсинг страницы {page}')
+        result = session.get(url, headers=headers, params=dict(params, pg=1))
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all('div', {'class': 'grid'})
+        for result in results:
+            vacancy = extract_job(result)
+            jobs_title.append(vacancy)
+    return jobs_title
